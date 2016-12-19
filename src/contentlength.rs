b5d95bbfa1ea1cd57909a0157d2daf64c032ca2b
antonin <antonin@MacBook-Pro-de-antonin.local>
2016-12-19 12:03:48 +0100

    Add module to get the content len. of a remote doc

---
+++ src/contentlength.rs
@@ -0,0 +1,23 @@
+use hyper::header::{ContentLength, Headers};
+use std::ops::Deref;
+
+/// Represents a byte, in `u64`.
+type Byte = u64;
+
+/// Trait to extend functionalities of the Headers type, from `hyper`
+pub trait GetContentLength {
+    /// Function to get the content length of a remote document.
+    /// The returned type is `Byte`.
+    fn get_content_length(&self) -> Option<Byte>;
+}
+
+impl GetContentLength for Headers {
+    /// Function to get the `content-length` container, from a given header.
+    /// This function returns an Option that contains a `Byte` type.
+    fn get_content_length(&self) -> Option<Byte> {
+        if self.has::<ContentLength>() {
+            return Some(*self.get::<ContentLength>().unwrap().deref())
+        }
+        None
+    }
+}
