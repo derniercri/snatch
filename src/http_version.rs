50da87b68f6c6d96ad520aee588c713e41b94942
Jean-Serge Monbailly <jean-serge@derniercri.io>
2016-12-23 21:07:01 +0100

    Update comment

--- src/http_version.rs
+++ src/http_version.rs
@@ -1,8 +1,10 @@
 use hyper::version::HttpVersion;
 
+/// Trait used to check HTTP Version
+///
+/// This trait is used to validate that a given HTTP Version match specific need.
 pub trait ValidateHttpVersion {
-    /// Check the HttpVersion is at least 1.1 to enable chunks download.
-    ///
+    /// Validate that the current HttpVersion is at least 1.1 to be able to download chunks.
     fn greater_than_http_11(&self) -> bool;
 }
 
@@ -10,7 +12,6 @@ impl ValidateHttpVersion for HttpVersion {
     /// Check the given HttpVersion.
     ///
     /// This version should be at least 1.1 to allow chunks downloading.
-    ///
     fn greater_than_http_11(&self) -> bool {
         self >= &HttpVersion::Http11
     }
