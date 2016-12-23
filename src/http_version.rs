ed511a39502df0c38065014bb00d3a396d217930
Jean-Serge Monbailly <jean-serge@derniercri.io>
2016-12-23 09:22:48 +0100

    Add doc-test

--- src/http_version.rs
+++ src/http_version.rs
@@ -1,5 +1,23 @@
 use hyper::version::HttpVersion;
 
+/// Check the given HttpVersion.
+///
+/// This version should be at least 1.1 to allow chunks downloading.
+///
+/// # Examples
+///
+/// ```
+/// extern crate hyper;
+/// extern crate snatch;
+///
+/// fn main() {
+///     use hyper::version::HttpVersion;
+///     use snatch::http_version::is_valid_http_version;
+///
+///     is_valid_http_version(HttpVersion::Http11);
+/// }
+/// ```
+///
 pub fn is_valid_http_version(v: HttpVersion) -> bool {
     v >= HttpVersion::Http11
 }
