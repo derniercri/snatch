9c2059db072b2436e30086e5686e4eb611df83c0
Jean-Serge Monbailly <jean-serge@derniercri.io>
2016-12-23 09:11:22 +0100

    Add unit tests for checking HTTP version

--- tests/http_version_test.rs
+++ tests/http_version_test.rs
@@ -1,8 +1,28 @@
+extern crate hyper;
+extern crate snatch;
 
 #[cfg(test)]
-mod test {
+mod test_http_versions {
+    use hyper::version::HttpVersion;
+    use snatch::http_version::is_valid_http_version;
+
     #[test]
-    fn validate_http_version() {
-        unimplemented!()
+    fn test_version_09() {
+        assert!(!is_valid_http_version(HttpVersion::Http09))
+    }
+
+    #[test]
+    fn test_version_10() {
+        assert!(!is_valid_http_version(HttpVersion::Http10))
+    }
+
+    #[test]
+    fn test_version_11() {
+        assert!(is_valid_http_version(HttpVersion::Http11))
+    }
+
+    #[test]
+    fn test_version_20() {
+        assert!(is_valid_http_version(HttpVersion::Http20))
     }
 }
