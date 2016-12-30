6fb721fd7b82262bacfc077dcab66b02e14d8282
k0pernicus <antonin@derniercri.io>
2016-12-30 18:32:23 +0100

    Change the definition of the function

--- src/write.rs
+++ src/write.rs
@@ -5,10 +5,7 @@ use std::io::Write;
 
 /// This function takes as parameter a file name and a vector of chaunks.
 /// The function will create a file, and append to the buffer each chunk.
-pub fn write_file(name_file: &str, chunks: &SChunks) -> Result<(), Error> {
-    
-    // Create the file - check if it doesn't exists
-    let mut local_file = File::create(name_file).unwrap();
+pub fn write_file(local_file: &mut File, chunks: &SChunks) -> Result<(), Error> {
 
     // Get the access to the chunks
     let chunks_m = chunks.lock().unwrap();
