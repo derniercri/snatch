124813eb95aec39844e1c7c723254a0da7962b14
k0pernicus <antonin.carette@gmail.com>
2017-01-21 01:30:12 +0100

    Update Byte type to Bytes

--- src/contentlength.rs
+++ src/contentlength.rs
@@ -1,21 +1,21 @@
 use hyper::header::{ContentLength, Headers};
 use std::ops::Deref;
 
-use Byte;
+use Bytes;
 
 /// Trait to extend functionalities of the Headers type, from `hyper`
 pub trait GetContentLength {
     /// Function to get the content length of a remote document.
-    /// The returned type is `Byte`.
-    fn get_content_length(&self) -> Option<Byte>;
+    /// The returned type is `Option<Bytes>`.
+    fn get_content_length(&self) -> Option<Bytes>;
 }
 
 impl GetContentLength for Headers {
     /// Function to get the `content-length` container, from a given header.
-    /// This function returns an Option that contains a `Byte` type.
-    fn get_content_length(&self) -> Option<Byte> {
+    /// This function returns an Option that contains a `Bytes` type.
+    fn get_content_length(&self) -> Option<Bytes> {
         if self.has::<ContentLength>() {
-            return Some(*self.get::<ContentLength>().unwrap().deref())
+            return Some(*self.get::<ContentLength>().unwrap().deref());
         }
         None
     }
