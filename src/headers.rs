96de6694ec973b71fd989a74e4b90518df6c9825
k0pernicus <antonin.carette@gmail.com>
2017-03-12 14:30:57 +0100

    Rename functions in headers

--- src/headers.rs
+++ src/headers.rs
@@ -8,23 +8,23 @@ use Bytes;
 pub trait HeadersSupporting {
     /// Function to get the content length of a remote document.
     /// The returned type is `Option<Bytes>`.
-    fn get_content_length(&self) -> Option<Bytes>;
+    fn get_contentlength_header_content(&self) -> Option<Bytes>;
     /// Function to know if the server supports the AcceptRanges option.
     /// This function returns a boolean value.
-    fn support_partialcontent(&self) -> bool;
+    fn support_acceptranges_header(&self) -> bool;
 }
 
 impl HeadersSupporting for Headers {
     /// Function to get the `content-length` container, from a given header.
     /// This function returns an Option that contains a `Bytes` type.
-    fn get_content_length(&self) -> Option<Bytes> {
+    fn get_contentlength_header_content(&self) -> Option<Bytes> {
         if self.has::<ContentLength>() {
             return Some(*self.get::<ContentLength>().unwrap().deref());
         }
         None
     }
     /// Function to know if the server supports the AcceptRanges option.
-    fn support_partialcontent(&self) -> bool {
+    fn support_acceptranges_header(&self) -> bool {
         self.has::<AcceptRanges>()
     }
 }
