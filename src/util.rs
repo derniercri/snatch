85a26636b0ef6eb69224526506b57004342ee1db
k0pernicus <antonin.carette@gmail.com>
2017-06-06 19:20:53 +0200

    Replace println statements with macros from src/logs

--- src/util.rs
+++ src/util.rs
@@ -1,18 +1,14 @@
-use ansi_term::Style;
-
 use std::io;
 use std::io::Write;
 
-pub fn prompt_user(style: Style, prompt: &str) -> String {
-    print!("{} ", style.paint(prompt));
-    io::stdout()
-        .flush()
-        .expect("[ERROR] Couldn't flush stdout!");
+pub fn prompt_user(prompt: &str) -> String {
+    warning!(prompt);
+    io::stdout().flush().expect("Couldn't flush stdout!");
 
     let mut user_input = String::new();
     io::stdin()
         .read_line(&mut user_input)
         .ok()
-        .expect("[ERROR] Couldn't read line!");
+        .expect("Couldn't read line!");
     String::from(user_input.trim())
 }
